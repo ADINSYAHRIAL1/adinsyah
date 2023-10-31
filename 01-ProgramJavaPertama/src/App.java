7bbbcae27f060a92ee5f9b09c5cf3161f2fa5b06
ADINSYAHRIAL1 <146052113+ADINSYAHRIAL1@users.noreply.github.com>
2023-10-31 13:18:31 +0800

    kuliah algo tgl 31

---
+++ 01-ProgramJavaPertama/src/App.java
@@ -0,0 +1,36 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       //Program Biodata Mahasiswa
+       //deklarasi variable
+       String nama;
+       String nim;
+       String tgl_lahir;
+       String alamat;
+       float berat_badan;
+       float tinggi_badan;
+
+       // mengisi variable
+       nama = "Adin";
+       nim = "23241051";
+       tgl_lahir = "19 April 2004";
+       alamat = "janapria";
+       berat_badan = 65.5f;
+       tinggi_badan = 172.5f;
+
+       // cetak biodata
+       System.out.println(====================);
+       System.out.println("Nama : " + nama);
+       System.out.println("NIM : " + nim);
+       System.out.println("Tanggal Lahir : " + tgl_lahir);
+       System.out.println("Alamat : " + alamat);
+       System.out.println("Berat Badan : " + berat_badan);
+       System.out.println("Tinggi Badan : " + tinggi_badan);
+       System.out.println("==================");
+
+
+
+
+
+
+    }
+}
